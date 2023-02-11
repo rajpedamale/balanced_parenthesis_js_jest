edc7887b97f4b9ad823c6ab6d4d7daa6277057af
xlu4PQ <xlu4PQ@cyber-dojo.org>
2023-02-11 21:53:10 +0000

    7 ran tests, predicted green, got green

--- files/balanced_parenthesis.js
+++ files/balanced_parenthesis.js
@@ -1,8 +1,22 @@
 'use strict';
 
 function isBalanced(parentheses) {
-  if (!parentheses) return true;
-  return false;
+    if (!parentheses) return true;
+
+    let balanced = false;
+    const openStack = [];
+
+    [...parentheses].forEach(letter => {
+        if (letter === '{') {
+            openStack.push(letter);
+        } else if (letter === '}') {
+            if (openStack.pop() === '{') {
+                balanced = true;
+            }
+        }
+    })
+
+    return balanced;
 }
 
 module.exports = isBalanced;
