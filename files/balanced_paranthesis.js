7aca53702cfa8229c806cd99c6b8ba81672932cf
xlu4PQ <xlu4PQ@cyber-dojo.org>
2023-02-11 21:40:43 +0000

    3 ran tests, predicted red, got red

--- files/balanced_paranthesis.js
+++ files/balanced_paranthesis.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function isBalanced() {
+function isBalanced(parantheses) {
   return false;
 }
 
--- files/balanced_paranthesis.test.js
+++ files/balanced_paranthesis.test.js
@@ -4,6 +4,13 @@ const isBalanced = require('./balanced_paranthesis');
 
 describe('Balanced Paranthesis check', () => {
   it('should return a boolean', () => {
-    expect(typeof isBalanced()).toEqual('boolean');
+    expect(typeof isBalanced('')).toEqual('boolean');
   });
+
+  it('should return true for empty arguments', () => {
+    expect(isBalanced('')).toBeTruthy();
+    expect(isBalanced()).toBeTruthy();
+    expect(isBalanced(null)).toBeTruthy();
+  });
+
 });
